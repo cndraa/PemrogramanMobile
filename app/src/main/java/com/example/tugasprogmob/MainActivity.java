3e92f72d0db2888620a3d6f16f0f3426e7da91b2
didimus candra <didimus.candra@si.ukdw.ac.id>
2019-09-03 18:23:44 +0700

    Button Dan Edit Text

--- app/src/main/java/com/example/tugasprogmob/MainActivity.java
+++ app/src/main/java/com/example/tugasprogmob/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.tugasprogmob;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.mainActivityTextView);
+        textView.setText(R.string.Test_untuk_Update_View);
+
+        Button myBtn = (Button) findViewById(R.id.button1);
+        myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText myEditText = (EditText) findViewById(R.id.editText1);
+                Log.d("TugasProgmob",myEditText.getText().toString());
+            }
+        });
+
+        myBtn.setOnClickListener(myBtnClickListener);
     }
+
+    private View.OnClickListener myBtnClickListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view) {
+            EditText myEditText = (EditText)findViewById(R.id.editText1);
+            Log.d("TugasProgmob",myEditText.getText().toString());
+        }
+    };
+
+
+
 }
