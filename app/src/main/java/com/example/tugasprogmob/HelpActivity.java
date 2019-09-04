8f1d2bd0c3402a063574cbd1f6efb731cd0b22e0
didimus candra <didimus.candra@si.ukdw.ac.id>
2019-09-04 18:55:42 +0700

    Pertemuan2

--- app/src/main/java/com/example/tugasprogmob/HelpActivity.java
+++ app/src/main/java/com/example/tugasprogmob/HelpActivity.java
@@ -3,6 +3,7 @@ package com.example.tugasprogmob;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -11,7 +12,12 @@ public class HelpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
+        TextView txtView = findViewById(R.id.textView2);
+        txtView.setText("Candra Ganteng");
+
+        Bundle b = getIntent().getExtras();
+        String tmpString = b.getString("helpString");
+
+        txtView.setText(tmpString);
     }
-
-
 }
--- app/src/main/java/com/example/tugasprogmob/MainActivity.java
+++ app/src/main/java/com/example/tugasprogmob/MainActivity.java
@@ -12,58 +12,60 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*@Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("abc","test");
+        super.onSaveInstanceState(outState);
+    }*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView) findViewById(R.id.mainActivityTextView);
-        textView.setText(R.string.Test_untuk_Update_View);
+        TextView textView = findViewById(R.id.mainActivityTextView);
+        final EditText myEditText = findViewById(R.id.editText1);
+        Button myBtn = findViewById(R.id.button1);
+        Button btnHelp = findViewById(R.id.helpButton);
 
-        Button myBtn = (Button) findViewById(R.id.button1);
+
+        textView.setText(R.string.Test_untuk_Update_View);
         myBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText myEditText = (EditText) findViewById(R.id.editText1);
-                Log.d("TugasProgmob",myEditText.getText().toString());
+                Log.d("Progmob A", myEditText.getText().toString());
             }
         });
 
-        myBtn.setOnClickListener(myBtnClickListener);
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HelpActivity.class); //intent untuk berpindah activity
+                startActivity(intent);
+            }
+        });
 
-        Button helpBtn = (Button)findViewById(R.id.helpButton);
-        helpBtn.setOnClickListener(helpButtonListener);
+        btnHelp.setOnClickListener(helpButtonListener);
 
         if(savedInstanceState != null){
-            Log.d("TugasProgmob",savedInstanceState.getString("QWERTY"));
+            Log.d("Progmob A",savedInstanceState.getString("abc"));
         }
     }
 
-    private View.OnClickListener myBtnClickListener = new View.OnClickListener()
-    {
-        @Override
-        public void onClick(View view) {
-            EditText myEditText = (EditText)findViewById(R.id.editText1);
-            Log.d("TugasProgmob",myEditText.getText().toString());
-        }
-    };
-
-    private View.OnClickListener helpButtonListener = new View.OnClickListener()
-    {
+    private View.OnClickListener helpButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             Intent intent = new Intent(MainActivity.this,HelpActivity.class);
+
+            Bundle b = new Bundle();
+
+            EditText myEditText = (EditText)findViewById(R.id.editText1);
+            b.putString("helpString",myEditText.getText().toString());
+            intent.putExtras(b);
+
             startActivity(intent);
         }
     };
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("QWERTY","Test1");
-        super.onSaveInstanceState(outState);
-    }
-
-
-
 }
